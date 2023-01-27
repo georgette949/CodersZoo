1bd9aef9931efdd263d5904dd5c81dcf7a1bd801
IrisGomis <gomis.f.iris@gmai.com>
2023-01-27 14:29:30 +0100

    fixed 1 error in branch CrearAnimal

--- class/CrearAnimal.php
+++ class/CrearAnimal.php
@@ -3,50 +3,51 @@ require "ClassZoo.php";
 
 class CrearAnimal extends ClassZoo
 {
-    private $host = "localhost:127.0.0.1";
-    private $user = "root@localhost";
+    private $host = "127.0.0.1";
+    private $user = "root";
     private $dbname = "coderszoo";
+    private $contrasenyaDB = '';
     private $conn;
 
     public function __construct()
     {
-        $this->conn = new mysqli($this->host, $this->user, $this->dbname);
+        $this->conn = new mysqli($this->host, $this->user, $this->contrasenyaDB, $this->dbname);
     }
 
-    // public function insert($nombre, $status)
-    // {
-    //     $stmt = $this->conn->prepare("INSERT INTO tabla (Nombre, Status) VALUES (?, ?)");
-    //     $stmt->bind_param("sii", $nombre, $status);
-    //     $stmt->execute();
-    //     $stmt->close();
-    // }
+    public function insert($nombre, $status)
+    {
+        $stmt = $this->conn->prepare("INSERT INTO tabla (Nombre, Status) VALUES (?, ?)");
+        $stmt->bind_param("sii", $nombre, $status);
+        $stmt->execute();
+        $stmt->close();
+    }
 }
 
 
 // Insertar los datos
 
-$conexion = mysqli_connect("localhost", "root", "", "db_coderszoofood");
-	$sql = "INSERT INTO `coderszoofood` (`Nombre`, `Status`) VALUES
-		('Unicornio', 'Alimentado'),
-        ('Minotauro', 'No Alimentado'),
-        ('Phoenix', 'No Alimentado'),
-        ('Dragón', 'Alimentado'),
-		('Hipogrifo', 'Alimentado');";
-	$resultado = mysqli_query($conexion, $sql);
+// $conexion = mysqli_connect("localhost", "root", "", "db_coderszoofood");
+// 	$sql = "INSERT INTO `coderszoofood` (`Nombre`, `Status`) VALUES
+// 		('Unicornio', 'Alimentado'),
+//         ('Minotauro', 'No Alimentado'),
+//         ('Phoenix', 'No Alimentado'),
+//         ('Dragón', 'Alimentado'),
+// 		('Hipogrifo', 'Alimentado');";
+// 	$resultado = mysqli_query($conexion, $sql);
 
 
 
-// $nombres = [
-//     ["Nombre" => "Unicornio", "Status" => "Alimentado"],
-//     ["Nombre" => "Hipogrifo", "Status" => "No alimentado"],
-//     ["Nombre" => "Minotauro", "Status" => "No alimentado"],
-//     ["Nombre" => "Phoenix", "Status" => "Alimentado"],
-//     ["Nombre" => "Dragón", "Status" => "Alimentado"]
-// ];
-// $db = new CrearAnimal();
-// foreach($nombres as $nombre){
-//     $db->insert($nombre['Nombre'], $status['Status']);
-// }
+$nombres = [
+    ["Nombre" => "Unicornio", "Status" => "Alimentado"],
+    ["Nombre" => "Hipogrifo", "Status" => "No alimentado"],
+    ["Nombre" => "Minotauro", "Status" => "No alimentado"],
+    ["Nombre" => "Phoenix", "Status" => "Alimentado"],
+    ["Nombre" => "Dragón", "Status" => "Alimentado"]
+];
+$db = new CrearAnimal();
+foreach($nombres as $nombre){
+    $db->insert($nombre['Nombre'], $status['Status']);
+}
 
 try {
     $conexion = new PDO("mysql:localhost:127.0.0.1", "root@localhost");
