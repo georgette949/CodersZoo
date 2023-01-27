ed2db342f7f9acbdbbe7ea4c7d7d858d0f756f1c
TaniaPandal <psiqonautas@gmail.com>
2023-01-27 12:36:16 +0100

    second steps

--- class/Delete.php
+++ class/Delete.php
@@ -3,37 +3,37 @@
 require "ClassZoo.php";
 
 class Delete extends ClassZoo {
-      
+// Atributos
+   public  $servername = "localhost/coderszoo";
+   public  $dbname = "coderszoo";
 
-  public function delete($nombre,$status){
-     
-    private $db;
+// atri-metodo
+   public $conn = new mysqli($servername, $dbname);
+
+// metodo
+   public function checkConnection(){
+     if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+        }
+        echo "Connected successfully";
+    }  
+    
+   //metodo
+  public function delete($bdname, $nombre, $status){
 
     public function __construct($db_connection)
     {
         $this -> db = $db_connection;
     }
-    
-    public function delete($nombre) {
-        $query = "DELETE FROM coderszoofood WHERE Nombre = $nombre";
     }
 
-  }
+    //metodo
+    public function delete($nombre) {
+        $query = "DELETE FROM coderszoofood WHERE Nombre = $nombre";
+    } 
 
 }
-//class DeleteElement {
-  //  private $db;
 
-   // public function __construct($db_connection) {
-   //     $this->db = $db_connection;
-   // }
-
-   // public function delete($table, $id) {
-   //     $query = "DELETE FROM $table WHERE id = :id";
-   //     $stmt = $this->db->prepare($query);
-   //     $stmt->bindParam(':id', $id);
-   //     return $stmt->execute();
-   // }
 
 
 
